06450346b075e4e8c3caae20a9b4c72bb7ad40fa
Johann284 <johann_ruth@estudante.sesisenai.org.br>
2024-04-17 12:49:13 -0300

    changing page and functioning JavaScript

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,50 @@
-usuarios = []
+var login = false; //define por padrão inpossível a troca de tela.
 
+
+function adquirirDados(){ //adquirindo dados digitados
+    var nome = document.getElementById('nome').value;
+    var senha = document.getElementById('senha').value;
+}
+
+
+
+// identificando valores de login
+function Usuario(nome, senha) { 
+    this.nome =  nome,
+    this.senha = senha
+}
+
+const usuario1 = new Usuario("johann","senha123");
+
+var Usuarios = [usuario1];
+
+function checkPassword(senha){
+    for (let i = 0; i < Usuario.length; i++) {
+        if (senha = Usuarios[i].senha) {
+            return true;
+        } 
+    }
+    return false;
+}
+
+function checkUsuario(nome){
+    for (let i = 0; i < Usuario.length; i++) {
+        if (senha = Usuarios[i].nome) {
+            return true;
+        } 
+    }
+    return false;
+}
+
+
+// caso o usuario for real ele permite a troca de tela
 function entrar(){
-
+    adquirirDados();
+    if (checkPassword(senha) == false) {
+        return false;
+    }
+    if (checkUsuario(nome) == false) {
+        return false;
+    }
+    window.location.href = "tela_oi.html";
 }
