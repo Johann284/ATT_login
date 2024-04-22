6055e63872f9605e94b40de8e4382f2628a5ffa0
Johann284 <johann_ruth@estudante.sesisenai.org.br>
2024-04-22 11:57:19 -0300

    fiz tudo funcionar!

--- scripts/script.js
+++ scripts/script.js
@@ -1,50 +1,68 @@
-var login = false; //define por padrão inpossível a troca de tela.
-
-
 function adquirirDados(){ //adquirindo dados digitados
-    var nome = document.getElementById('nome').value;
-    var senha = document.getElementById('senha').value;
+    var nomeUsuario = document.getElementById('nome').value;
+    var senhaUsuario = document.getElementById('senha').value;
+    return new Usuario(nomeUsuario , senhaUsuario);
 }
 
-
-
 // identificando valores de login
-function Usuario(nome, senha) { 
-    this.nome =  nome,
-    this.senha = senha
+function Usuario(nomeUsuario, senhaUsuario) { 
+    this.nome =  nomeUsuario,
+    this.senha = senhaUsuario
 }
 
 const usuario1 = new Usuario("johann","senha123");
 
-var Usuarios = [usuario1];
+var totalUsuarios = [usuario1];
 
-function checkPassword(senha){
-    for (let i = 0; i < Usuario.length; i++) {
-        if (senha = Usuarios[i].senha) {
+console.log(totalUsuarios);
+
+function checkUsuario(nomeUsuario){
+    for (let i = 0; i < totalUsuarios.length; i++) {
+        if (nomeUsuario === totalUsuarios[i].nome) {
+            console.log("nome");
             return true;
         } 
     }
+    console.log(nomeUsuario);
+    console.log("não nome");
     return false;
 }
 
-function checkUsuario(nome){
-    for (let i = 0; i < Usuario.length; i++) {
-        if (senha = Usuarios[i].nome) {
+
+function checkPassword(senhaUsuario){
+    for (let i = 0; i < totalUsuarios.length; i++) {
+        if (senhaUsuario == totalUsuarios[i].senha) {
+            console.log("senha");
             return true;
         } 
     }
+    console.log("não senha");
     return false;
 }
-
 
 // caso o usuario for real ele permite a troca de tela
 function entrar(){
-    adquirirDados();
-    if (checkPassword(senha) == false) {
+    Usuario = adquirirDados();
+    nomeUsuario = document.getElementById('nome').value;
+    localStorage.setItem('nome', Usuario.nome); 
+    senhaUsuario = document.getElementById('senha').value;
+    localStorage.setItem('senha', Usuario.senha); 
+    if (checkUsuario(Usuario.nome) == false) {
+        window.location.href = "index.html";
         return false;
     }
-    if (checkUsuario(nome) == false) {
+    if (checkPassword(Usuario.senha) == false) {
+        window.location.href = "index.html";
         return false;
     }
     window.location.href = "tela_oi.html";
 }
+
+
+if (document.URL.includes("tela_oi.html")) {
+    let nomeUsuario = localStorage.getItem('nome');
+    document.getElementById("nome_usuario").textContent = nomeUsuario;
+    let senhaUsuario = localStorage.getItem('senha');
+    document.getElementById("senha_usuario").textContent = senhaUsuario;
+}
+
